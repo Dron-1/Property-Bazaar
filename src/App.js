bbd1f2e0b2d99499fd39a6f0a01fd7bc2cb81a56
Dron-1 <dronchandna.333@gmail.com>
2023-02-07 21:18:18 +0530

    Added new page Profile

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Offers from './pages/Offers';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import Profile from './pages/Profile';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path = '/' element = {<Explore />} />
-          <Route path = '/profile' element = {<SignIn />} />
+          <Route path = '/profile' element = {<Profile />} />
           <Route path = '/offers' element = {<Offers />} />
           <Route path = '/sign-in' element = {<SignIn />} />
           <Route path = '/sign-up' element = {<SignUp />} />
